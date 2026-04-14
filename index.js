59f7f185419d7914d334684c4d35a7edef4349ab
Ariel Muhuri <ariel.muhuri@student.moringaschool.comgit config --global user.email ariel.muhuri@student.moringaschool.com>
2026-04-14 22:01:37 +0300

    Completed lab assessment

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+require('datejs');
 
+function mergeUsernames(...args) {
+    let allUsers = [];
+    for (const usersArray of args) {
+        allUsers = [...allUsers, ...usersArray];
+    }
+    return allUsers;
+}
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+    for (const usersArray of args) {
+        combinedObject.users = [...combinedObject.users, ...usersArray];
+    }
+
+    combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+    return combinedObject;
+}
+
+// Example usage
+const group1 = ["Lincon", "Ariel"];
+const group2 = ["Alvin", "Charity"];
+const group3 = ["Kerry"];
+
+console.log(mergeUsernames(group1, group2, group3));
+// ["alice", "bob", "charlie", "diana", "eve"]
+
+console.log(combineUsers(group7, group10, group11));
+// {
+//   users: ["Henry", "Ariel", "Eva", "Shello", "Bosco"],
+//   merge_date: "4/14/2026"
+// }
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+    mergeUsernames,
+    combineUsers
 };
